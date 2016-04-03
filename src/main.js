05bc2772add766582fe77dc518faec0cbb671f9a
prestonion <me@prestonion.me>
2016-04-03 14:04:21 -0500

    Setup

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import {app, BrowserWindow} from 'electron';
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if(process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/view/index.html');
+  
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
